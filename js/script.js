c38c3f7b5d94d3b3b1d6cfb012e71928993da2f3
taqwa-ouda <takwa.mansour.ouda@gmail.com>
2022-01-18 19:53:39 +0200

    coffee-task

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+function display()
+{
+  var  x =  document.getElementsByClassName('cart-shopping');
+    if (x.style.display==="none")
+    {
+        x.style.display ="block";
+    }else{
+        x.style.display ="none";
+
+    }
+}
